cc4a5c8819920945168ed5b6f3511b5c5aed9133
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2021-03-09 13:00:59 +0100

    added select distinct

--- function.php
+++ function.php
@@ -2,7 +2,7 @@
 
 /* DarkIntaqt 2021 JSONSQL - (https://darkintaqt.com | https://github.com/darkintaqt)  */
 
-function jsonsql($sql = false, $json = null) {
+function jsonsql($sql = false, $json = null, $databasename = "json") {
   if($sql == false || $json == null) {
     return "jsonsql(\"SQL\",\"JSON or PHP Array\"); - No SQL or JSON given. ";
   }
@@ -13,6 +13,51 @@ function jsonsql($sql = false, $json = null) {
     }
   } elseif (!is_array($json)) {
     return "\$json is not a json string or an array. ";
+  }
+  if(substr(strtolower($sql),0,6) == "select") {
+    $n = 1;
+    switch(str_word_count(strtolower($sql),1,'*')[$n]) {
+
+
+      /* SELECT * */
+
+      case "*":
+        $n++;
+        if(str_word_count(strtolower($sql),1,'*')[$n] == "from") {
+          return $json;
+        } else {
+          return "No database given. The default Database name is \"json\". (SELECT * FROM json)";
+        }
+        break;
+
+
+      /* DISTINCT */
+
+      case "distinct":
+        $n++;
+        $a = [];
+        foreach ($json as $key => $value) {
+          $q = false;
+          foreach ($ as $k => $v) {
+            if($json[$key][] == $a[$k][]) {
+              $q = true;
+            }
+          }
+          if($q != true) {
+            $a[$key] = $json[$key];
+          }
+        }
+        break;
+
+
+      /* CUSTOM ROW NAME */
+
+      default:
+        return "Unknown command";
+        break;
+    }
+  } else {
+    return "Unknown Method - Currently available methods are: \"SELECT\"";
   }
 }
 
